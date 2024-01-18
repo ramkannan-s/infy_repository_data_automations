21cdce6f0ef5c19fb9156120c9b277efa6716318
RAMKANNAN S <ramkannan1991@gmail.com>
2024-01-18 16:30:28 +0530

    Update createProjects.sh

--- create_resources/createProjects.sh
+++ create_resources/createProjects.sh
@@ -6,8 +6,9 @@ JPD_AUTH_TOKEN="${2:?please enter the access token}"
 projectname="${3:?please enter the project name}"
 projectid="${4:?please enter the project count}"
 userid="${5:?please enter the username to make as projectadmin}"
+storage_quota_bytes="${6:?please enter the storage for project in bytes}"
 
-project_template_cmd="cat projectTemplate.json | jq '.display_name = \"$projectname\"' | jq '.project_key = \"$projectid\"' > project.json"
+project_template_cmd="cat projectTemplate.json | jq '.display_name = \"$projectname\"' | jq '.project_key = \"$projectid\" | jq '.storage_quota_bytes = \"$storage_quota_bytes\"' > project.json"
 eval "$project_template_cmd"
 curl -XPOST -H "Authorization: Bearer ${JPD_AUTH_TOKEN}" "$SOURCE_JPD_URL/access/api/v1/projects" -d @project.json -s -H 'Content-Type: application/json'
 echo -e ""
