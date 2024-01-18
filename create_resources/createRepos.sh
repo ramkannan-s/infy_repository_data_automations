019f94377f2768f99e11666d9af54dd1221c8812
RAMKANNAN S <ramkannan1991@gmail.com>
2024-01-18 16:55:00 +0530

    Update createRepos.sh

--- create_resources/createRepos.sh
+++ create_resources/createRepos.sh
@@ -5,8 +5,15 @@ repotype="${1:?please enter the repo type. ex - local/remote/virtual/federated}"
 packagetype="${2:?please enter the package type type. ex - docker/maven/helm}"
 repo_key="${3:?please enter reponame}"
 
-replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-local-template.json"
+if [ "$repo_key" = "local" ] ; then
+  replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-template.json"
+elif [ "$repo_key" = "remote" ] ; then
+  replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-template.json"
+elif [ "$repo_key" = "virtual" ] ; then
+  replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-template.json"
+elif [ "$repo_key" = "federated" ] ; then
+  replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-template.json"
+fi
 eval "$replace_cmd" > repository-update-template.json
 cat repository-update-template.json
-#jf rt repo-delete $repo_key --quiet
 jf rt repo-create repository-update-template.json
