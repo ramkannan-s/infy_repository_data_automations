8de22edf9f08d2f15562d37ecc2e456bbf256181
RAMKANNAN S <ramkannan1991@gmail.com>
2024-01-18 18:23:16 +0530

    Update createRepos.sh

--- create_resources/createRepos.sh
+++ create_resources/createRepos.sh
@@ -4,6 +4,11 @@
 repotype="${1:?please enter the repo type. ex - local/remote/virtual/federated}"
 packagetype="${2:?please enter the package type type. ex - docker/maven/helm}"
 repo_key="${3:?please enter reponame}"
+url="${4:?please enter for of Remote repo}"
+username="${5:?please enter username for Remote repo}"
+password="${6:?please enter password for Remote repo}"
+defaultDeploymentRepo="${7:?please enter Default Deployment Repo for Virtual repo}"
+repositories="${8:?please enter Repositories List for Virtual repo}"
 
 if [ "$repo_key" = "local" ] ; then
   replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-template.json"
