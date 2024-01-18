6d1e4ed4897e8ec111b25975c7ed6863aac21dc7
RAMKANNAN S <ramkannan1991@gmail.com>
2024-01-18 14:44:48 +0530

    Update createProjects.sh

--- create_resources/createProjects.sh
+++ create_resources/createProjects.sh
@@ -5,11 +5,11 @@ projectname="${4:?please enter the project name}"
 projectid="${5:?please enter the project count}"
 userid="${6:?please enter the username to make as projectadmin}"
 
-project_template_cmd="cat projectTemplate.json | jq '.display_name = \"$projectnameprefix-$item\"' | jq '.project_key = \"$projectidprefix$item\"' > project-$item.json"
+project_template_cmd="cat projectTemplate.json | jq '.display_name = \"$projectname\"' | jq '.project_key = \"$projectid\"' > project.json"
 eval "$project_template_cmd"
-curl -XPOST -H "Authorization: Bearer ${JPD_AUTH_TOKEN}" "$SOURCE_JPD_URL/access/api/v1/projects" -d @project-"$item".json -s -H 'Content-Type: application/json'
+curl -XPOST -H "Authorization: Bearer ${JPD_AUTH_TOKEN}" "$SOURCE_JPD_URL/access/api/v1/projects" -d @project.json -s -H 'Content-Type: application/json'
 echo -e ""
-echo -e "Adding user $userprefix-site-b-user-$item to $projectnameprefix-$item"
-user_template_cmd="cat projectUserAdd.json | jq '.name = \"$userprefix-site-b-user-$item\"' > user-$item.json"
+echo -e "Adding user $userid to $projectname"
+user_template_cmd="cat projectUserAdd.json | jq '.name = \"$userid\"' > user.json"
 eval "$user_template_cmd"
-curl -XPUT -H "Authorization: Bearer ${JPD_AUTH_TOKEN}" "$SOURCE_JPD_URL/access/api/v1/projects/$projectidprefix$item/users/$userprefix-site-b-user-$item" -d @user-$item.json -s -H 'Content-Type: application/json'
+curl -XPUT -H "Authorization: Bearer ${JPD_AUTH_TOKEN}" "$SOURCE_JPD_URL/access/api/v1/projects/$projectid/users/$userid" -d @user.json -s -H 'Content-Type: application/json'
