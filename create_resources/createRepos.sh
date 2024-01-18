769f5ad488dbc58b363821c118b8cc3853701228
RAMKANNAN S <ramkannan1991@gmail.com>
2024-01-18 10:17:43 +0530

    Update createRepos.sh

--- create_resources/createRepos.sh
+++ create_resources/createRepos.sh
@@ -1,1 +1,12 @@
-#
+#! /bin/bash
+
+### Get Arguments
+repotype="${1:?please enter the repo type. ex - local/remote/virtual/federated}"
+packagetype="${2:?please enter the package type type. ex - docker/maven/helm}"
+repo_key="${3:?please enter reponame}"
+
+replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\"' repository-create-template.json"
+eval "$replace_cmd" > repository-update-template.json
+cat repository-update-template.json
+#jf rt repo-delete $repo_key --quiet
+jf rt repo-create repo-update-template.json
