e0ae0c01c31d01869b58277b2bc72449b74d3233
Ramkannan S <ramkannans@jfrog.com>
2024-01-24 20:05:53 +0530

    jan 24 push - 3

--- create_resources/createVirtualRepo.sh
+++ create_resources/createVirtualRepo.sh
@@ -9,7 +9,16 @@ repo_key="${3:?please enter reponame}"
 defaultDeploymentRepo="${4:?please enter Default Deployment Repo for Virtual repo}"
 repositories="${5:?please enter Repositories List for Virtual repo}"
 
-replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\" | .defaultDeploymentRepo = \"$defaultDeploymentRepo\" | .repositories = \"$repositories\"' repository-template.json"
+repositories_arry_string=""
+IFS=";"
+for repo in $repositories; do
+    repositories_arry_string+="\"$repo\", "
+done
+repositories_data=$(echo "[ $repositories_arry_string" | rev | cut -c3- | rev )]
+echo "$repositories_data"
+
+replace_cmd="jq '.key = \"$repo_key\" | .packageType = \"$packagetype\" | .rclass = \"$repotype\" | .defaultDeploymentRepo = \"$defaultDeploymentRepo\" | del(.members) | .repositories = $repositories_data' repository-template.json"
 eval "$replace_cmd" > repository-update-template.json
+gsed -i -e 's/\\//g' repository-update-template.json
 cat repository-update-template.json
-jf rt repo-create repository-update-template.json
+jf rt curl -XPUT "/api/repositories/$repo_key" -d @repository-update-template.json -H 'Content-Type: application/json'
